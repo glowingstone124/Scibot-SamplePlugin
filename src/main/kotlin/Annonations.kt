2136710003521d6c1784f9731d24fe5c41009155
glowingstone124 <hanserofficial@outlook.com>
2024-04-28 22:08:49 +0800

    new

---
+++ src/main/kotlin/Annonations.kt
@@ -0,0 +1,11 @@
+package ind.glowingstone
+
+class Annonations {
+    @Retention(AnnotationRetention.RUNTIME)
+    @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+    annotation class PlainHandler(val type: MessageConstructor.Types = MessageConstructor.Types.PLAIN)
+
+    @Retention(AnnotationRetention.RUNTIME)
+    @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+    annotation class PrivateHandler(val type: MessageConstructor.Types = MessageConstructor.Types.PLAIN)
+}
