968bffdf2a2a467d25071c7c4335821826e9054f
glowingstone124 <hanserofficial@outlook.com>
2024-05-01 20:33:32 +0800

    support plugin main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
 import java.util.logging.Level
 
 class PluginMain : Plugin {
-
+    /*
+    This is the sample plugin of SciBot.
+    Define Main class and Plugin Name in resources/plugin.yml
+    use @PlainHandler to create an event handler.
+    implements Plugin interface to create a main class.
+     */
     override fun start(logger: SimpleLogger) {
         logger.log("hello,World", Level.INFO)
     }
